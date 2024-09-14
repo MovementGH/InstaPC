7dbe5c0efe493188f0508c4469ed8c2248094a4b
tanishwangoo <tanishwangoo@vt.edu>
2024-09-14 12:03:52 -0400

    User redirection
    A logged in user redirects to the /home page. If not logged in, app redirects to sign up/login.

--- ui/src/app/auth/authscreen.tsx
+++ ui/src/app/auth/authscreen.tsx
@@ -1,31 +1,14 @@
 "use client"
 import { withAuthInfo, useRedirectFunctions, useLogoutFunction, WithAuthInfoProps } from '@propelauth/react'
 
+import { useRouter } from "next/router";
+import { useEffect } from 'react';
+
 const AuthUI = withAuthInfo((props: WithAuthInfoProps) => {
     const logoutFunction = useLogoutFunction();
-    const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
-    if (props.isLoggedIn) { 
-        return (
-                <div className="bg-white p-8 rounded-lg shadow-lg text-center w-96">
-                    <div className="mb-6 bg-teal-100 p-4 rounded-md">
-                        <h2 className="text-xl font-semibold text-teal-800">Welcome!</h2>
-                        <p className="text-gray-600 mt-2">You are logged in as <span className="font-medium">{props.user.email}</span></p>
-                    </div>
-                    <button
-                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors w-full mb-2"
-                        onClick={() => redirectToAccountPage()}
-                    >
-                        Account
-                    </button>
-                    <button
-                        className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-colors w-full"
-                        onClick={() => logoutFunction(true)}
-                    >
-                        Logout
-                    </button>
-                </div>
-        );
-    } else {
+    const { redirectToOrgPage, redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
+
+    if (!props.isLoggedIn) {
         return (
                 <div className="bg-white p-8 rounded-lg shadow-lg text-center w-96">
                     <div className="mb-6 p-4 rounded-md">
