68e23565994f511789b01b577e23516e96ef12c6
tanishwangoo <tanishwangoo@vt.edu>
2024-09-14 10:25:14 -0400

    trying authscreen integration

---
+++ ui/src/app/auth/authscreen.tsx
@@ -0,0 +1,52 @@
+"use client"
+import { withAuthInfo, useRedirectFunctions, useLogoutFunction, WithAuthInfoProps } from '@propelauth/nextjs/client'
+
+const YourApp = withAuthInfo((props: WithAuthInfoProps) => {
+    const logoutFunction = useLogoutFunction();
+    const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
+    if (props.isLoggedIn) { 
+        return (
+                <div className="bg-white p-8 rounded-lg shadow-lg text-center w-96">
+                    <div className="mb-6 bg-teal-100 p-4 rounded-md">
+                        <h2 className="text-xl font-semibold text-teal-800">Welcome!</h2>
+                        <p className="text-gray-600 mt-2">You are logged in as <span className="font-medium">{props.user.email}</span></p>
+                    </div>
+                    <button
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors w-full mb-2"
+                        onClick={() => redirectToAccountPage()}
+                    >
+                        Account
+                    </button>
+                    <button
+                        className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-colors w-full"
+                        onClick={() => logoutFunction(true)}
+                    >
+                        Logout
+                    </button>
+                </div>
+        );
+    } else {
+        return (
+                <div className="bg-white p-8 rounded-lg shadow-lg text-center w-96">
+                    <div className="mb-6 p-4 rounded-md">
+                        <h2 className="text-xl font-semibold text-orange-500">Hello!</h2>
+                        <p className="text-gray-600 mt-2">Login with <span className=" font-semibold text-green-800">PropelAuth</span></p>
+                    </div>
+                    <button
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors w-full mb-2"
+                        onClick={() => redirectToLoginPage()}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-colors w-full"
+                        onClick={() => redirectToSignupPage()}
+                    >
+                        Signup
+                    </button>
+                </div>
+        );
+    }
+});
+
+export default YourApp
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Computer } from "lucide-react";
 import VMList from "@/components/vm-list";
+import YourApp from "./auth/authscreen";
 
 export default function Home() {
   return (
     <main className="flex flex-col grow justify-center items-center p-4 w-full">
       <div className="max-w-7xl w-full mt-12">
         <h1 className="font-bold text-xl mb-4 text-gray-300">Available PCs <Computer className="inline ml-1 size-5 mb-1"/></h1>
+        <YourApp/>
         <VMList />
       </div>
     </main>
