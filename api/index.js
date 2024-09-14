b339e6508f57ae373cd63b04737e5e77c64ede74
Movement <48330137+MovementGH@users.noreply.github.com>
2024-09-13 21:08:44 -0400

    Setup docker

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to your Express.js app!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
