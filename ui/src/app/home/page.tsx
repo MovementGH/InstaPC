c0c987c955c2b983ebcf3c8e66adda76c88bc784
coltfox <coltonfox2004@gmail.com>
2024-09-14 14:30:29 -0400

    Add user sidebar

--- ui/src/app/home/page.tsx
+++ ui/src/app/home/page.tsx
@@ -8,6 +8,7 @@ import {
   useLogoutFunction,
   WithAuthInfoProps,
 } from "@propelauth/react";
+import Nav from "@/components/nav";
 const Home = (props: WithAuthInfoProps) => {
   const [logoutbttn, setlogoutbttn] = useState(props.isLoggedIn);
   const logoutFunction = useLogoutFunction();
@@ -19,31 +20,17 @@ const Home = (props: WithAuthInfoProps) => {
     return null;
   }
   return (
-    <main className="flex flex-col grow justify-center items-center p-4 w-full">
-      <div className="max-w-7xl w-full mt-12">
-        <div>
-          {logoutbttn ? (
-            <div className="flex items-center space-x-4">
-              <p className="text-green-500">
-                Current Session: {props.user?.email}
-              </p>
-              <button
-                onClick={() => logoutFunction(true)}
-                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
-              >
-                Logout
-              </button>
-            </div>
-          ) : (
-            <></>
-          )}
+    <div className="flex grow">
+      <Nav />
+      <main className="flex flex-col items-center p-4 w-full">
+        <div className="max-w-7xl w-full mt-12">
+          <h1 className="font-bold text-xl mb-4 text-gray-300">
+            Available PCs <Computer className="inline ml-1 size-5 mb-1" />
+          </h1>
+          <VMList />
         </div>
-        <h1 className="font-bold text-xl mb-4 text-gray-300">
-          Available PCs <Computer className="inline ml-1 size-5 mb-1" />
-        </h1>
-        <VMList />
-      </div>
-    </main>
+      </main>
+    </div>
   );
 };
 
---
+++ ui/src/components/nav.tsx
@@ -0,0 +1,30 @@
+import { CircleUser, House, LogOut } from "lucide-react";
+import Link from "next/link";
+import { useAuthInfo, useLogoutFunction } from "@propelauth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function Nav() {
+  const authInfo = useAuthInfo();
+  const logout = useLogoutFunction()
+
+  return (
+    <nav className="flex flex-col grow items-center gap-8 p-6">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="outline-none"><CircleUser className="size-7 text-gray-400 hover:text-gray-300" /></DropdownMenuTrigger>
+        <DropdownMenuContent className="mt-1 ml-4">
+          <DropdownMenuLabel className="font-medium text-xs">Currently logged in as:</DropdownMenuLabel>
+          <DropdownMenuLabel>{authInfo.user?.email}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="hover:cursor-pointer" onClick={() => logout(true)}><LogOut className="inline mr-1 size-4"/>Log Out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
+  );
+}
